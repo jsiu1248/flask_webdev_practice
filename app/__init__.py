8661026a6fa5c9aaa6af2ff3ee9cfccf1ff32c12
Jonathan Siu <jsiu1248@gmail.com>
2022-08-08 22:27:46 -0700

    moved __init__ file

---
+++ app/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+import os
+from config import configs
+basedir = os.path.abspath(os.path.dirname(__file__))
+db = SQLAlchemy()
+bootstrap = Bootstrap()
+"""
+Database Models...
+LoginManager User Loader...
+"""
+def create_app(config_name='default'):
+    app = Flask(__name__)
+    # old way: app.config.from_pyfile('config.py')
+    config_class = configs[config_name]
+
+    # configuration settings are loaded through the from_object() method
+    app.config.from_object(config_class)
+
+    # he configuration class init_app() static method
+    # is called to do any remaining setup for the app
+    config_class.init_app(app)
+    @app.route('/')
+    def index():
+         # Code for index route...
+        """
+        Your other routes and error handlers here...
+        """
+    return app
+
+new_app = create_app()
+
+
