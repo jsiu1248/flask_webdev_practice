3f47c94c4bac66ca828739ef3f257b1e9877d80a
Jonathan Siu <jsiu1248@gmail.com>
2022-08-08 22:27:23 -0700

    added processor

--- ragtime.py
+++ ragtime.py
@@ -1,4 +1,4 @@
-from app import app
+# from app import app
 from app import create_app
 import os
 
@@ -6,6 +6,10 @@ import os
 app = create_app(os.environ.get("FLASK_CONFIG") or "default")
 
 # [PROBABLY SOMETHING WITH MIGRATE/SHELL CONTEXT PROCESSOR]
+"""@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db)"""
+
 # set FLASK_APP = ragtime.py
 
 #TODO
