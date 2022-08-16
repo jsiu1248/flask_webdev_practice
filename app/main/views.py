7d1f4e7f2c5381662cb4fce3c3398f6dd36e6273
Jonathan Siu <JonathanSiu@jonathans-mbp.lan>
2022-08-15 22:35:21 -0700

    added models and __init__.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,12 @@
 
 # can't just import like normal because we are in a Blueprint
 # have to do it in a way that it records the actions
-
-@app.route('/', methods=["GET", "POST"])
+from . import main
+@main.route('/', methods=["GET", "POST"])
 def index():
     form = NameForm()
     # more code for index route...
     return render_template("index.html", name=name)
-@app.route('/user/<username>')
+@main.route('/user/<username>')
 def user(username):
     return render_template("user.html", user_name=username)
