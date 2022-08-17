553f3c6fa79be986801c4a3cc7572b8842e007e7
Jonathan Siu <JonathanSiu@jonathans-mbp.lan>
2022-08-16 21:08:05 -0700

    added form and bootstrap modules

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+from flask import render_template, Blueprint
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+
 
 main = Blueprint('main', __name__, template_folder='templates',
 url_prefix='main')
--- app/main/views.py
+++ app/main/views.py
@@ -2,11 +2,13 @@
 # can't just import like normal because we are in a Blueprint
 # have to do it in a way that it records the actions
 from . import main
+
 @main.route('/', methods=["GET", "POST"])
 def index():
     form = NameForm()
     # more code for index route...
     return render_template("index.html", name=name)
+
 @main.route('/user/<username>')
 def user(username):
     return render_template("user.html", user_name=username)
