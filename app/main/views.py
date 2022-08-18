1fd41ff42359842fac43a8533b1a67c8eac9e6af
Jonathan Siu <jsiu1248@gmail.com>
2022-08-17 18:16:44 -0700

    tested import. need to figure out correct syntax so main doesn't need to be explicitly imported

--- app/main/views.py
+++ app/main/views.py
@@ -2,12 +2,16 @@
 # can't just import like normal because we are in a Blueprint
 # have to do it in a way that it records the actions
 from . import main
+#from flask import Blueprint, render_template
+#from .forms import NameForm
+
+main=Blueprint('main',__name__)
 
 @main.route('/', methods=["GET", "POST"])
 def index():
     form = NameForm()
     # more code for index route...
-    return render_template("index.html", name=name)
+    return render_template("index.html")
 
 @main.route('/user/<username>')
 def user(username):
