03eb8c5a7bbc367a4c98a42860efd371e0c65b5b
Jonathan Siu <jsiu1248@gmail.com>
2022-08-21 21:40:55 -0700

    removing hard coding blueprint into views

--- app/main/views.py
+++ app/main/views.py
@@ -2,10 +2,7 @@
 # can't just import like normal because we are in a Blueprint
 # have to do it in a way that it records the actions
 from . import main
-#from flask import Blueprint, render_template
-#from .forms import NameForm
 
-# main=Blueprint('main',__name__)
 
 @main.route('/', methods=["GET", "POST"])
 def index():
