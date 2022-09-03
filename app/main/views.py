f8bfbf07dddd6a21ab9ec7a9668d268f1bdfc9e7
Jonathan Siu <jsiu1248@gmail.com>
2022-09-03 09:15:00 -0700

    fixing index

--- app/main/views.py
+++ app/main/views.py
@@ -27,7 +27,7 @@ def user(username):
 def login():
     return render_template("login.html")
 
-main.route('/register')
+@main.route('/register')
 def register():
     return render_template('register.html')
 
