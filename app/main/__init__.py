d8759a263b63d06d094e3032945bc427e03893df
Jonathan Siu <jsiu1248@gmail.com>
2022-09-27 18:19:42 -0700

    imported Permission from models

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,6 @@
 from flask import render_template, Blueprint
 from flask_bootstrap import Bootstrap
+from ..models import Permission
 
 # if the line below is there because it would be stuck
 # because it didn't exist yet main
