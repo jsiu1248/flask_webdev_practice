d47253901fa14fda82527236f05b9411989a7f40
Jonathan Siu <jsiu1248@gmail.com>
2022-10-09 15:02:28 -0700

    added slug logic to fake

--- app/fake.py
+++ app/fake.py
@@ -27,6 +27,8 @@ def users(count=20):
         try:
             db.session.commit()
             i += 1
+            for c in Composition.query.all():
+                c.generate_slug()
         except IntegrityError:
             db.session.rollback()
 
