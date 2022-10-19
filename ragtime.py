c749de804c8eb0daa89a9ab93d6b53e64cb7caf4
Jonathan Siu <jsiu1248@gmail.com>
2022-10-18 22:06:49 -0700

    added to shell context

--- ragtime.py
+++ ragtime.py
@@ -1,6 +1,7 @@
 # from app import app
-from app import create_app
+from app import create_app, db
 import os
+from app.models import Composition, Role, User, Follow
 
 # need to set ENV FLASK_CONFIG
 app = create_app(os.environ.get("FLASK_CONFIG") or "default")
@@ -8,7 +9,7 @@ app = create_app(os.environ.get("FLASK_CONFIG") or "default")
 # [PROBABLY SOMETHING WITH MIGRATE/SHELL CONTEXT PROCESSOR]
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db)
+    return dict(db=db, Role=Role, User=User, Composition=Composition, Follow=Follow)
 
 # set FLASK_APP = ragtime.py
 
