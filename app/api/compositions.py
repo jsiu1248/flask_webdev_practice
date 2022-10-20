ee223fc208ae855d140a2a852dc2b851260a017c
Jonathan Siu <jsiu1248@gmail.com>
2022-10-19 23:21:10 -0700

    endpoint for editing compositions

--- app/api/compositions.py
+++ app/api/compositions.py
@@ -4,6 +4,8 @@ from ..models import Composition
 from . import api
 from .. models import Permission
 from ..decorators import permission_required
+from .errors import forbidden
+from functools import wraps
 
 @api.route('/compositions/')
 def get_compositions():
@@ -27,3 +29,29 @@ def new_composition():
     # returns deserialized and again serialized composition and 201 and location header
     return jsonify(composition.to_json()), 201, \
         {'Location': url_for('api.get_composition', id=composition.id)}
+
+def permission_required(permission):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not g.current_user.can(permission):
+                return forbidden("Insufficient permissions")
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+
+@api.route('/compositions/<int:id>', methods=['PUT'])
+@permission_required(Permission.PUBLISH)
+def edit_composition(id):
+    composition = Composition.query.get_or_404(id)
+    if g.current_user != composition.artist and \
+            not g.current_user.can(Permission.ADMIN):
+        return forbidden('Insufficient permissions')
+    import json
+    put_json = json.loads(request.json)
+    composition.release_type = put_json.get('release_type', composition.release_type)
+    composition.title = put_json.get('release_type', composition.title)
+    composition.description = put_json.get('description', composition.description)
+    db.session.add(composition)
+    db.session.commit()
+    return jsonify(composition.to_json())
